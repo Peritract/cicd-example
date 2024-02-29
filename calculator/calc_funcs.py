4f8e6ea044ade8577fc5725a06775c219b9085a5
Dan <peritract@hotmail.co.uk>
2024-02-29 10:26:56 +0000

    Initial functions

---
+++ calculator/calc_funcs.py
@@ -0,0 +1,9 @@
+"""Functions for actually calculating things."""
+
+def add(a, b):
+    
+    return round(a + b, 2)
+
+def subtract(a, b):
+
+    return round(a - b, 2)
--- calculator/main.py
+++ calculator/main.py
@@ -1,5 +1,9 @@
 """Runs the calculator functions."""
 
+from calc_funcs import add, subtract
+
 if __name__ == "__main__":
 
     print("Calculating...")
+    print(add(0.2, 0.1))
+    print(subtract(0.2, 0.1))
